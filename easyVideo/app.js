6b774f12b3005bb45b810e5a35b10bff417fa186
Igor <53260102+lr0pb@users.noreply.github.com>
2020-08-23 15:30:29 +0300

    Create app.js

---
+++ easyVideo/app.js
@@ -0,0 +1,28 @@
+const config = {
+  timeForFrame: 100
+}
+let frames = []
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function getFiles(e) {
+  frames = []
+  for (let file of e.files) {
+    let frame = URL.createObjectURL(file)
+    frames.push(frame)
+  }
+}
+
+async function playFiles() {
+  for (frame of frames) {
+    $('#viewBox').style.backgroundImage = `url(${frame})`
+    await new Promise( (res) => {
+      setTimeout(res, config.timeForFrame)
+    })
+  }
+}
+
+$('input').addEventListener('change', getFiles)
+$('#start').addEventListener('click', playFiles)
