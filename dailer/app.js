9e28ddecc5c4ac7bbf0a06becb03f698ec1e04a0
Igor <53260102+lr0pb@users.noreply.github.com>
2022-05-10 15:54:34 +0300

    Fix import and add IDB initial use

--- dailer/app.js
+++ dailer/app.js
@@ -1,6 +1,16 @@
-import pages from './pages.js'
+import { pages } from './pages.js'
+import IDB from './IDB.js'
+
+const db = new IDB('dailer', 1, [
+  {
+    name: 'things', index: {keyPath: 'id'}
+  }, {
+    name: 'days', index: {keyPath: 'date'}
+  }
+]);
 
 const globals = {
+  db,
   paintPage: (name) => {
     document.body.innerHTML = pages[name].page;
     pages[name].script(globals);
