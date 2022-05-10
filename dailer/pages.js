5259b61e0d452d7101d74111d585685baeb6bfd2
Igor <53260102+lr0pb@users.noreply.github.com>
2022-05-11 00:29:38 +0300

    AddThing actions is live

--- dailer/pages.js
+++ dailer/pages.js
@@ -15,7 +15,7 @@ const onboarding = {
 const planCreator = {
   page: `
     <h1>Add your daily things you will control</h1>
-    <div id="things"></div>
+    <div id="things" class="fullscreen"></div>
     <button id="addThing">Add thing</button>
   `,
   script: onPlanCreator
@@ -26,15 +26,21 @@ async function onPlanCreator (globals) {
     'click', () => globals.paintPage('thingCreator')
   );
   const things = await globals.db.getAll('things');
-  const thingsContainer = qs('#things')
+  const thingsContainer = qs('#things');
   if (!things.lenght) {
     thingsContainer.innerHTML = `<h3>There is nothing yet!</h3>`;
   } else for (let td of things) { // td stands for thingsData
     const thing = document.createElement('div');
     thing.className = 'thing';
+    thing.dataset.id = td.id;
     thing.innerHTML = `
-      <h4>${td.name}</h4>
+      <h3>${td.name}</h3>
+      <button data-action="edit" class="smolBtn">Edit</button>
+      <button data-action="delete" class="smolBtn">Delete</button>
     `;
+    thing.addEventListener('click', (e) => {
+      
+    })
     thingsContainer.append(thing);
   }
 }
@@ -42,15 +48,33 @@ async function onPlanCreator (globals) {
 const thingCreator = {
   page: `
     <h1>Add thing</h1>
-    
+    <input type="text" id="name" placeHolder="Enter your task you will add"></input>
+    <select id="period">
+      <option>One time only</option>
+      <option>Everyday</option>
+      <option>Every second day</option>
+      <option>Two over two</option>
+      <option>Only weekdays</option>
+      <option>On weekends<option>
+      <option>Custom period</option>
+    </select>
     <button id="saveThing">Save thing</button>
   `,
-  script: (globals) => {
-    qs('#saveThing').addEventListener(
-      'click', () => globals.paintPage('planCreator')
-    );
-  }
+  script: onSaveThing
 };
+
+function onSaveThing(globals) {
+  qs('#saveThing').addEventListener(
+    'click', () => {
+      globals.db.setItem('things', {
+        id: createId(),
+        name: String(qs('#name').textContent),
+        disabled: false
+      });
+      globals.paintPage('planCreator');
+    }
+  );
+}
 
 export const pages = {
   onboarding, planCreator, thingCreator
