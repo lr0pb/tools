a3c4068f3fb85fb675eb193c72aea232360f7bba
Igor <53260102+lr0pb@users.noreply.github.com>
2022-05-11 12:33:38 +0300

    Rename thing to task, fix task creation

--- dailer/pages.js
+++ dailer/pages.js
@@ -14,68 +14,98 @@ const onboarding = {
 
 const planCreator = {
   page: `
-    <h1>Add your daily things you will control</h1>
-    <div id="things" class="fullscreen"></div>
-    <button id="addThing">Add thing</button>
+    <h1>Add your daily tasks you will control</h1>
+    <div id="tasks" class="fullscreen"></div>
+    <button id="addTask">Add task</button>
   `,
   script: onPlanCreator
 };
 
-async function onPlanCreator (globals) {
-  qs('#addThing').addEventListener(
-    'click', () => globals.paintPage('thingCreator')
+async function onPlanCreator(globals) {
+  qs('#addTask').addEventListener(
+    'click', () => globals.paintPage('taskCreator')
   );
-  const things = await globals.db.getAll('things');
-  const thingsContainer = qs('#things');
-  if (!things.lenght) {
-    thingsContainer.innerHTML = `<h3>There is nothing yet!</h3>`;
-  } else for (let td of things) { // td stands for thingsData
-    const thing = document.createElement('div');
-    thing.className = 'thing';
-    thing.dataset.id = td.id;
-    thing.innerHTML = `
+  const tasks = await globals.db.getAll('tasks');
+  const tasksContainer = qs('#tasks');
+  if (!tasks.lenght) {
+    tasksContainer.innerHTML = `<h3>There is nothing yet!</h3>`;
+  } else for (let td of tasks) { // td stands for task's data
+    if (td.disabled) continue;
+    const task = document.createElement('div');
+    task.className = 'task';
+    task.dataset.data = JSON.stringify(td);
+    task.innerHTML = `
       <h3>${td.name}</h3>
       <button data-action="edit" class="smolBtn">Edit</button>
       <button data-action="delete" class="smolBtn">Delete</button>
     `;
-    thing.addEventListener('click', (e) => {
-      
-    })
-    thingsContainer.append(thing);
+    task.addEventListener('click', onTaskManageClick)
+    thingsContainer.append(task);
   }
 }
 
-const thingCreator = {
+aaync function onTaskManageClick(e) {
+  if (e.target.dataset.action == 'edit') {
+    globals.paintPage('taskCreator');
+    
+  } else if (e.target.dataset.action == 'delete') {
+    //await globals.db.deleteItem('tasks', this.dataset.id);
+    const td = JSON.parse(this.dataset.td);
+    td.disabled = true;
+    await globals.db.setItem('tasks', td);
+    this.remove();
+    globals.message({
+      state: 'success', text: 'Task deleted'
+    });
+  } else return;
+}
+
+const taskCreator = {
   page: `
-    <h1>Add thing</h1>
-    <input type="text" id="name" placeHolder="Enter your task you will add"></input>
+    <h1>Add task</h1>
+    <input type="text" id="name" placeHolder="Enter task you will control"></input>
     <select id="period">
       <option>One time only</option>
       <option>Everyday</option>
       <option>Every second day</option>
       <option>Two over two</option>
       <option>Only weekdays</option>
-      <option>On weekends<option>
+      <option>On weekends</option>
       <option>Custom period</option>
     </select>
-    <button id="saveThing">Save thing</button>
+    <button id="saveTask">Save task</button>
   `,
-  script: onSaveThing
+  script: onSaveTask
 };
 
-function onSaveThing(globals) {
+function onSaveTask(globals) {
   qs('#saveThing').addEventListener(
     'click', () => {
-      globals.db.setItem('things', {
-        id: createId(),
-        name: String(qs('#name').textContent),
-        disabled: false
-      });
-      globals.paintPage('planCreator');
+      try {
+        const task = {
+          id: createId(),
+          name: qs('#name').value,
+          disabled: false
+        };
+        globals.db.setItem('tasks', task);
+        globals.message({
+          state: 'success', text: 'Task added'
+        });
+        globals.paintPage('planCreator');
+      } catch (err) {
+        globals.message({
+          state: 'fail', text: 'Fill all fields'
+        });
+      };
     }
   );
 }
 
+function createId() {
+  const date = new Date();
+  return date.now().toString();
+}
+
 export const pages = {
-  onboarding, planCreator, thingCreator
+  onboarding, planCreator, taskCreator
 };
