f7dbbdf26442bca06f7724489c66934fbae46a38
Igor <53260102+lr0pb@users.noreply.github.com>
2022-05-29 19:22:42 +0300

    Add periodCreator

--- dailer/pages.js
+++ dailer/pages.js
@@ -4,7 +4,8 @@ import { settings } from './pages/settings.js'
 import { planCreator, tasksArchive } from './pages/taskLists.js'
 import { taskCreator } from './pages/taskCreator.js'
 import { taskInfo } from './pages/taskInfo.js'
+import { periodCreator } from './pages/periodCreator.js'
 
 export const pages = {
-  onboarding, main, settings, planCreator, taskCreator, tasksArchive, taskInfo
+  onboarding, main, settings, planCreator, taskCreator, tasksArchive, taskInfo, periodCreator
 };
